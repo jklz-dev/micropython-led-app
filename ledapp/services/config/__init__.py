82ae3240ec1cfcdc347782faf307cb10ac217396
Jared Spencer <jklz@me.com>
2023-12-17 16:02:26 -0700

    add configs

--- ledapp/services/config/__init__.py
+++ ledapp/services/config/__init__.py
@@ -1,5 +1,3 @@
 from .config_handler import ConfigHandler
 
 networkConfig = ConfigHandler("/config/network.json")
-deviceConfig = ConfigHandler("/config/device.json")
-mqttConfig = ConfigHandler("/config/mqtt.json")
