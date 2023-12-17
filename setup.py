15e3a85c56e277289072434ae7e2092431d70a08
Jared Spencer <jklz@me.com>
2023-12-17 14:15:14 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='micropython-ledapp',
+    version='0.0.1',
+    description='IOT Led App for MicroPython',
+
+    url='https://github.com/jklz-dev/micropython-ledapp',
+    author='Jared Spencer',
+    maintainer='Jared Spencer',
+    packages=find_packages()
+)
