ddcc4097541ba3ca862293bc01f0659b364ed7b0
Jared Spencer <jklz@me.com>
2023-12-17 18:59:50 -0700

    adjust run

--- ledapp/pixel.py
+++ ledapp/pixel.py
@@ -2,7 +2,7 @@ from .configs import deviceConfig, displayConfig
 from time import sleep_ms
 from machine import Pin
 from neopixel import NeoPixel
-import uasyncio
+import _thread
 
 
 class PixelHandler:
@@ -10,6 +10,7 @@ class PixelHandler:
     _pin: Pin
 
     def __init__(self):
+        self.config = deviceConfig
         config_pin = deviceConfig.pin
         config_total_pixels = deviceConfig.total_pixels
         self._pin = Pin(config_pin, mode=Pin.OUT)
@@ -19,11 +20,11 @@ class PixelHandler:
     def is_setup(self) -> bool:
         return self._pin is not None or self._neopixel is not None
 
-    async def _set_display_solid(self, color: tuple) -> None:
+    def _set_display_solid(self, color: tuple) -> None:
         self._neopixel.fill(color)
         self._neopixel.write()
 
-    async def _set_display_flash(self, color: tuple, speed: int) -> None:
+    def _set_display_flash(self, color: tuple, speed: int) -> None:
         is_on = True
         while (deviceConfig.display['speed'] is not None
                and deviceConfig.display['type'] == 'flash'
@@ -34,21 +35,19 @@ class PixelHandler:
                 self._neopixel.fill((0, 0, 0))
             self._neopixel.write()
             is_on = not is_on
-            await uasyncio.sleep_ms(speed)
-    async def async_set_display(self, display: dict) -> None:
+            sleep_ms(speed)
+
+    def set_display(self, display: dict):
+        deviceConfig.display = display
 
         if display['type'] is None:
             print('no display type set')
 
         elif display['type'] == 'solid':
-            uasyncio.create_task(self._set_display_solid(display['color']))
+            self._set_display_solid(display['color'])
 
         elif display['type'] == 'flash':
-            uasyncio.create_task(self._set_display_flash(display['color'], display['speed']))
-
-    def set_display(self, display: dict):
-        deviceConfig.display = display
-        uasyncio.get_event_loop().run_until_complete(self.async_set_display(display))
+            _thread.start_new_thread(self._set_display_flash, (display['color'], display['speed']))
 
 
 
