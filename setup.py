cd5a3dc8ff29b99d135a7ec37d7caf53bb2f8b69
Jared Spencer <jklz@me.com>
2023-12-20 21:07:47 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='micropython-ledapp',
-    version='0.4.9',
+    version='0.4.10',
     description='IOT Led App for MicroPython',
 
     url='https://github.com/jklz-dev/micropython-ledapp',
